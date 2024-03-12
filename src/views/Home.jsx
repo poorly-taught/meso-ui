13e664c4c6517de9e74db77e1b65ef01f770f53c
poorly-taught <nullcarter@gmail.com>
2024-03-12 11:03:09 -0500

    removed uneccessary placeholders

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,36 +1,40 @@
-import { CardHeader, Heading, Flex, Card, Box, Text, Circle} from "@chakra-ui/react";
+import { Flex, Card, Text, Circle } from "@chakra-ui/react";
 
 export default function Home() {
-    return (
-        <Flex bg='bg.1' direction={'column'} h={'100vh'} w={'100vw'} gap={4} pb={4}>
-            
-            <Flex bg='bg.1' direction={'row'} justify='center' align='center'>
-                <Box w="90%" h='80%' pr={4} display={'flex'} justifyContent={'flex-end'} bg="bg.2" alignItems={'center'}>
-                    <Text textStyle={'name'} fontSize={'30px'}> MESOGROWTH </Text>
-                </Box>
-            </Flex>
+  return (
+    <Flex bg="bg.1" direction={"column"} h={"100vh"} w={"100vw"} gap={4} pb={4}>
+      <Flex bg="bg.2" direction={"row"} justify="center" align="center">
+        <Text textStyle={"name"} fontSize={"30px"}>
+          {" "}
+          MESOGROWTH{" "}
+        </Text>
+      </Flex>
 
-            <Flex bg='bg.1' direction={'row'} h={'30%'} justify='center' align='center'>
-                <Card bg='bg.2' h='100%' w='90%'>
-                    <CardHeader>
-                        <Heading size='md'>Stop watch</Heading>
-                    </CardHeader>
-                </Card>
-            </Flex>
+      <Flex
+        bg="bg.1"
+        direction={"row"}
+        h={"30%"}
+        justify="center"
+        align="center"
+      >
+        <Card bg="bg.2" h="100%" w="95%"></Card>
+      </Flex>
 
-            <Flex bg='bg.1' direction={'row'} flex={1} justify='center' align='flex-start'>
-                <Card h='100%' w='90%' display={"flex"}>
-                    <CardHeader>
-                        <Heading size='md'>Calander</Heading>
-                    </CardHeader>
-                </Card>
-            </Flex>
+      <Flex
+        bg="bg.1"
+        direction={"row"}
+        flex={1}
+        justify="center"
+        align="flex-start"
+      >
+        <Card h="100%" w="95%" display={"flex"}></Card>
+      </Flex>
 
-            <Flex direction={'row'} w='100%' pr={5} justify={'flex-end'}>
-              <Circle size='40px' bg='bg.2' color='ft.1'>
-                T
-              </Circle>
-            </Flex>
-        </Flex>
-    )
+      <Flex direction={"row"} w="100%" pr={5} justify={"flex-end"}>
+        <Circle size="40px" bg="bg.2" color="ft.1">
+          T
+        </Circle>
+      </Flex>
+    </Flex>
+  );
 }
