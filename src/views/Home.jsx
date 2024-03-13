54493729d69b07f89b92d84b011b7f010a66f826
poorly-taught <nullcarter@gmail.com>
2024-03-13 00:33:40 -0500

    initial create program workflow list of programs

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -12,7 +12,19 @@ import {
   Circle,
 } from "@chakra-ui/react";
 
+import { useRef, useState, useEffect } from "react";
+
 export default function Home() {
+  const parentRef = useRef(null);
+  const [parentHeight, setParentHeight] = useState(0);
+
+  useEffect(() => {
+    if (parentRef.current) {
+      const height = parentRef.current.getBoundingClientRect().height;
+      setParentHeight(height);
+    }
+  }, [parentRef]);
+
   return (
     <Flex bg="bg.1" direction={"column"} h={"100vh"} w={"100vw"} gap={4} pb={4}>
       <Flex bg="bg.2" direction={"row"} justify="center" align="center">
@@ -40,20 +52,64 @@ export default function Home() {
         align="flex-start"
       >
         <Card h="100%" w="95%" display={"flex"}>
-          <Tabs isFitted colorScheme="bg.1">
-            <TabList mb="1em">
-              <Tab textStyle={'name'}>Programs</Tab>
-              <Tab textStyle={'name'}>Cycles</Tab>
+          <Tabs isFitted colorScheme="bg.2" h="100%">
+            <TabList mb="0" h="10%">
+              <Tab textStyle={"name"}>Programs</Tab>
+              <Tab textStyle={"name"}>Cycles</Tab>
             </TabList>
-            <TabPanels>
-              <TabPanel>
-                <Stack direction={'row'} justify={'center'}>
-                  <Button colorScheme="bg.1" variant="outline" size={"sm"}>
+            <TabPanels h="90%">
+              <TabPanel
+                h="100%"
+                display={"flex"}
+                flexDir={"column"}
+                flex={1}
+                p={5}
+                gap={"5px"}
+              >
+                <Flex direction={"row"} flex={1} ref={parentRef}>
+                  <Stack
+                    w="100%"
+                    maxH={`${parentHeight}px`}
+                    overflowY={"scroll"}
+                  >
+                    <Card mb="5px" h='80px' bg="bg.2" p={2.5} color={"bg.1"}>
+                      <Text>test</Text>
+                    </Card>
+                    <Card mb="5px" h='80px' bg="bg.2" p={2.5} color={"bg.1"}>
+                      <Text>test</Text>
+                    </Card>
+                    <Card mb="5px" h='80px' bg="bg.2" p={2.5} color={"bg.1"}>
+                      <Text>test</Text>
+                    </Card>
+                    <Card mb="5px" h='80px' bg="bg.2" p={2.5} color={"bg.1"}>
+                      <Text>test</Text>
+                    </Card>
+                    <Card mb="5px" h='80px' bg="bg.2" p={2.5} color={"bg.1"}>
+                      <Text>test</Text>
+                    </Card>
+                    <Card mb="5px" h='80px' bg="bg.2" p={2.5} color={"bg.1"}>
+                      <Text>test</Text>
+                    </Card>
+                    <Card mb="5px" h='80px' bg="bg.2" p={2.5} color={"bg.1"}>
+                      <Text>test</Text>
+                    </Card>
+                    <Card mb="5px" h='80px' bg="bg.2" p={2.5} color={"bg.1"}>
+                      <Text>test</Text>
+                    </Card>
+                  </Stack>
+                </Flex>
+                <Flex direction={"row"}>
+                  <Button
+                    colorScheme="bg.1"
+                    w="100%"
+                    variant="outline"
+                    size={"sm"}
+                  >
                     Create Program
                   </Button>
-                </Stack>
+                </Flex>
               </TabPanel>
-              <TabPanel>
+              <TabPanel h="100%" display={"flex"} flexDir={"column"} flex={1}>
                 <p>two!</p>
               </TabPanel>
             </TabPanels>
