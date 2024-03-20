32b1fb5a363d27a2998aaac7fa192e59cd16824c
poorly-taught <nullcarter@gmail.com>
2024-03-19 23:28:34 -0500

    ✨ (ProgramEditor.jsx): Refactor ProgramEditor component to improve readability and maintainability by restructuring ExerciseInput component and adding PropTypes validation.

--- src/views/components/ProgramEditor.jsx
+++ src/views/components/ProgramEditor.jsx
@@ -1,18 +1,64 @@
-import { useRef, useState } from 'react';
-import { Card, Stack, Text, Input, Textarea, Flex } from '@chakra-ui/react';
-
+import {
+  Card,
+  Stack,
+  Flex,
+  Text,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+} from "@chakra-ui/react";
+import PropTypes from "prop-types";
 
 export default function ProgramEditor({ exercises }) {
-
-    const ExerciseInput = ({ exercise }) => {
-        return <Card bg='bg.2' p={2} mb={1}>
+  const ExerciseInput = ({ exercise }) => {
+    return (
+      <Card direction="row" bg="bg.2" p={2} mb={1}>
+        <Flex direction="column" flex={2}>
+          <Text textStyle="name" fontSize={15}>
             {exercise.name}
-        </Card>
+          </Text>
+        </Flex>
+        <Flex direction="column" flex={1}>
+          <Flex direction="row">
+            <Flex dir="column">
+              <NumberInput size="sm" defaultValue={15} min={10} max={20}>
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </Flex>
+            <Flex dir="column">
+              <NumberInput size="sm" defaultValue={15} min={10} max={20}>
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </Flex>
+          </Flex>
+        </Flex>
+      </Card>
+    );
+  };
 
-    }
+  ExerciseInput.propTypes = {
+    exercise: PropTypes.object.isRequired,
+  };
 
-
-    return <Stack>
-        {Object.keys(exercises).map((e, i) => <ExerciseInput key={e} exercise={exercises[e]} />)}
+  return (
+    <Stack>
+      {Object.keys(exercises).map((e) => (
+        <ExerciseInput key={e} exercise={exercises[e]} />
+      ))}
     </Stack>
+  );
 }
+
+ProgramEditor.propTypes = {
+  exercises: PropTypes.object,
+};
