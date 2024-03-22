494b7a548cc789de6e737e231b63c42967da8da5
poorly-taught <nullcarter@gmail.com>
2024-03-21 23:25:05 -0500

    feat(ProgramEditor.jsx): add onChange prop to ExerciseInput component to handle exercise value changes feat(ProgramEditor.jsx): add useCallback hook to optimize value change function in ExerciseInput component feat(ProgramEditor.jsx): update ExerciseInput component to handle sets and reps changes feat(CreateProgramModal.jsx): add setsAndReps state to manage sets and reps data in CreateProgramModal component

--- src/views/components/ProgramEditor.jsx
+++ src/views/components/ProgramEditor.jsx
@@ -10,50 +10,76 @@ import {
   NumberDecrementStepper,
 } from "@chakra-ui/react";
 import PropTypes from "prop-types";
+import { useCallback } from "react";
 
-export default function ProgramEditor({ exercises }) {
-  const ExerciseInput = ({ exercise }) => {
-    return (
-      <Card direction="row" bg="bg.2" p={2} mb={1}>
-        <Flex direction="column" flex={2}>
-          <Text textStyle="name" fontSize={15}>
-            {exercise.name}
-          </Text>
-        </Flex>
-        <Flex direction="column" flex={1}>
-          <Flex direction="row">
-            <Flex dir="column">
-              <NumberInput size="sm" defaultValue={15} min={10} max={20}>
-                <NumberInputField />
-                <NumberInputStepper>
-                  <NumberIncrementStepper />
-                  <NumberDecrementStepper />
-                </NumberInputStepper>
-              </NumberInput>
-            </Flex>
-            <Flex dir="column">
-              <NumberInput size="sm" defaultValue={15} min={10} max={20}>
-                <NumberInputField size="sm" />
-                <NumberInputStepper size="sm">
-                  <NumberIncrementStepper size="sm" />
-                  <NumberDecrementStepper size="sm" />
-                </NumberInputStepper>
-              </NumberInput>
-            </Flex>
+const ExerciseInput = ({ exercise, onChange }) => {
+  const onValueChange = useCallback((exercise, value, type) => {
+    onChange(prev => {
+      if (prev[exercise._id]) {
+        return {
+          ...prev,
+          [exercise._id]: {
+            ...prev[exercise._id],
+            exercise,
+            [type]: parseInt(value)
+          }
+        }
+
+      }
+
+      if (!prev[exercise._id]) {
+        return {
+          ...prev,
+          [exercise._id]: {
+            exercise,
+            [type]: parseInt(value)
+          }
+        }
+      }
+    })
+  }, [onChange])
+  
+  return (
+    <Card direction="row" bg="bg.2" p={2} mb={1}>
+      <Flex direction="column" flex={2}>
+        <Text textStyle="name" fontSize={15}>
+          {exercise.name}
+        </Text>
+      </Flex>
+      <Flex direction="column" flex={1}>
+        <Flex direction="row">
+          <Flex dir="column">
+            <NumberInput size="sm" defaultValue={0} min={0} max={50} onChange={(value) => onValueChange(exercise, value, 'sets')}>
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+          </Flex>
+          <Flex dir="column">
+            <NumberInput size="sm" defaultValue={0} min={0} max={50} onChange={(value) => onValueChange(exercise, value, 'reps')}>
+              <NumberInputField size="sm" />
+              <NumberInputStepper size="sm">
+                <NumberIncrementStepper size="sm" />
+                <NumberDecrementStepper size="sm" />
+              </NumberInputStepper>
+            </NumberInput>
           </Flex>
         </Flex>
-      </Card>
-    );
-  };
+      </Flex>
+    </Card>
+  );
+};
 
-  ExerciseInput.propTypes = {
-    exercise: PropTypes.object.isRequired,
-  };
-
+ExerciseInput.propTypes = {
+  exercise: PropTypes.object.isRequired,
+};
+export default function ProgramEditor({ exercises, onChange }) {
   return (
     <Stack>
       {Object.keys(exercises).map((e) => (
-        <ExerciseInput key={e} exercise={exercises[e]} />
+        <ExerciseInput key={e} exercise={exercises[e]} onChange={onChange} />
       ))}
     </Stack>
   );
--- src/views/workflows/CreateProgramModal.jsx
+++ src/views/workflows/CreateProgramModal.jsx
@@ -23,10 +23,12 @@ export default function CreateProgramModal({ onClose }) {
   const initialRef = useRef(null);
   const finalRef = useRef(null);
   const isOpen = useRef(true);
-
+  
   const { get: getExercises, isLoading: exercisesIsLoading, data: exercisesData } = useExercises();
-  const [selectedExercises, setSelectedExercises] = useState({})
   const [currentStep, setCurrentStep] = useState(0)
+
+  const [selectedExercises, setSelectedExercises] = useState({})
+  const [setsAndReps, setSetsAndReps] = useState({})
 
   return (
     <Modal initialFocusRef={initialRef} finalFocusRef={finalRef} isOpen={isOpen} onClose={onClose}>
@@ -40,23 +42,19 @@ export default function CreateProgramModal({ onClose }) {
         </ModalHeader>
 
         {currentStep === 0 && <>
-          <Card p={2} ml={2} mr={2} mb={1} bg='bg.2'>
-            <Text textStyle='name' fontSize={15}>Search and Select Exercises</Text>
-            <Input
-              size='sm'
-              name='exerciseSearchBar'
-              focusBorderColor="bg.1"
-              variant="outline"
-            />
-            <Text mt={1} textStyle='name' fontSize={15}>Number of exercises selected {Object.keys(selectedExercises).length}</Text>
-          </Card>
           <ModalBody pb={5} p={2} overflowY={"scroll"}>
             <ExercisesList exercises={exercisesData.items} selectedExercises={selectedExercises} onSelect={setSelectedExercises} />
           </ModalBody></>}
 
         {currentStep === 1 &&
           <ModalBody pb={5} p={2} overflowY={'scroll'}>
-            <ProgramEditor exercises={selectedExercises} />
+            <ProgramEditor exercises={selectedExercises} onChange={setSetsAndReps}/>
+          </ModalBody>
+        }
+
+        {currentStep === 2 && 
+          <ModalBody>
+            {/* <ProgramReview setSetsAndReps={setsAndReps}/> */}
           </ModalBody>
         }
 
